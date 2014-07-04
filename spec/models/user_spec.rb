01fd2a9838f49b6137404d04cddb4535414adf3e
rishabhwadhawan <fire.blackfire@gmail.com>
2014-07-04 18:50:44 +0530

    added users model and password authentication for signup

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,90 @@
+require 'spec_helper'
+
+describe User do
+	before do
+  	@user  = User.new(name: "Rishabh", email: "a@b.com", password: "rishabh1", password_confirmation: "rishabh1")
+	end
+
+subject{@user}
+	it{should respond_to(:name)}
+	it{should respond_to(:email)}
+	it {should respond_to(:password_digest)}
+	it {should respond_to(:password)}
+	it {should respond_to(:password_confirmation)}
+	it{should respond_to(:authenticate)}
+	it{should be_valid}
+
+describe "password not short" do
+	before { @user.password = @user.password_confirmation = "a" * 5 }
+	it{should be_invalid}
+end
+
+describe "password authentication of user" do
+	before{@user.save}
+	let(:found_user) { User.find_by(email: @user.email)}
+	describe "valid password of user" do
+	it{ should eq found_user.authenticate(@user.password)}
+	end
+	describe"invalid password" do
+	let(:invalid_password_user) { found_user.authenticate("invalid")}
+	it {should_not eq invalid_password_user}
+	it { expect(invalid_password_user).to be_false }
+end
+end
+
+describe "When name is not present" do
+	before{@user.name = " "}
+	it {should_not be_valid}
+end
+
+describe "when password is not present" do
+	before do
+		@user = User.new(name: "Rishabh", email: "a@b.com", password: " ", password_confirmation: " ")
+	it {should_not be_valid} 
+end
+end
+
+describe "the password should match" do
+	before{ @user.password_confirmation ="mismatch"}
+it{should_not be_valid}
+end
+
+describe "When email not valid" do
+ 	before{@user.email = " "}
+ 	it {should_not be_valid}
+end
+describe " Limited name length of user" do
+	before{ @user.name = "a"*31}
+	it {should_not be_valid}
+end
+describe " Limited email length of user" do
+	before{ @user.email = "a"*51}
+	it {should_not be_valid}
+end
+describe "Emails are not valid" do
+	it "should be invalid" do
+	emailadd = %w[afe@gae+afw, egE@aefa_qwdq]
+	emailadd.each do |invalid_address|
+	@user.email = invalid_address
+	expect(@user).not_to be_valid
+end
+end
+end
+describe "Emails are valid" do
+	it"should be valid" do
+	emailadd = %w[efw@asd.com]
+	emailadd.each do |valid_address|
+	@user.email = valid_address
+	expect(@user).to be_valid
+end
+end
+end
+describe "Unique emails" do
+	before do
+		user_with_same_email = @user.dup
+		user_with_same_email.email = @user.email.upcase
+		user_with_same_email.save
+	end
+	it {should_not be_valid}
+end
+end 
