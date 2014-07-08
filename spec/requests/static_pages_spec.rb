1f5f38782dda2c5436f75dbd99b3d845e121f210
rishabhwadhawan <fire.blackfire@gmail.com>
2014-07-08 13:25:31 +0530

    added user profile

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,6 +15,7 @@ describe "Static pages" do
       let(:page_title) {''}
       it_should_behave_like "all static pages"
       it { should_not have_title('| Home')}
+      it { should have_content('Ankit')}
   end
 
   describe "Help page" do
